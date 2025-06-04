1386a8c46a84bae0ea99831adc6288d3746cea40
Logan Harbour <loganhharbour@gmail.com>
2025-06-04 16:17:28 -0600

    Finish material

--- src/materials/UnobtaniumMaterial.C
+++ src/materials/UnobtaniumMaterial.C
@@ -22,5 +22,5 @@ UnobtaniumMaterial::UnobtaniumMaterial(const InputParameters & parameters)
 void
 UnobtaniumMaterial::computeQpProperties()
 {
-  _k[_qp] = 1.0;
+  _k[_qp] = 1.0 / std::sqrt(1 + _grad_u * _grad_u);
 }
